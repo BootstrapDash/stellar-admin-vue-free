bdb65875d2a9942acbe0f4c0dce11b5b44a1388d
bootstrapdash-team <git@bootstrapdash.com>
2020-11-06 15:03:35 +0530

    footer links

--- template/demo_1/src/layout/partials/Footer.vue
+++ template/demo_1/src/layout/partials/Footer.vue
@@ -2,8 +2,8 @@
   <section class="app-footer">
     <footer class="footer">
       <div class="container-fluid clearfix">
-        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2019 <a href="http://www.bootstrapdash.com/" target="_blank">Bootstrapdash</a>. All rights reserved.</span>
-        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i></span>
+        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://www.bootstrapdash.com/" target="_blank">bootstrapdash.com </a>2020</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="https://www.bootstrapdash.com/vue-admin-templates/" target="_blank">Vue template</a> made by BootstrapDash.</span>
       </div>
     </footer>
   </section>
